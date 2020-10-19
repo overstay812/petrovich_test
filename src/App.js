53be846b8ba4248d53c50142394c518ce7355c5a
Konstantin Surnin <overstay812@gmail.com>
2020-10-19 22:23:53 +0300

    do parsing data system

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Temlate from './Temlate';
 function App() {
 
+const [database, setDatabase] = useState([])
 useEffect(()=>{
-fetch("src/products.json")
+
+fetch('http://localhost:3001/products')
 .then(response=> response.json())
-.then(data=>console.log(data))
+.then(data=> setDatabase(data))
 .catch(e=> console.log(e))
 },[])
 
   return (
     <div className="App">
-     <Temlate/>
+      { database.map((item, index) => <Temlate {...item} key={`${item.code} + ${index}`}/> )}
     </div>
   );
 }
